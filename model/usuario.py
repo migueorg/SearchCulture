1a62409ff144a0f64e381de48cb9fdcd130f165a
arguellesm <m.arguelles28@gmail.com>
2021-10-20 14:05:57 +0200

    close migueorg/SearchCulture#7

---
+++ model/usuario.py
@@ -0,0 +1,25 @@
+import datetime
+
+
+class Usuario:
+    """Usuario de la aplicación SearchCulture.
+
+    Atributos
+    ---------
+    tipo : int
+        Tipo de usuario.
+    alta : datetime
+        Fecha de alta del usuario en la aplicación.
+    region : str
+        Región geográfica del usuario.
+    intereses : list[str]
+        Intereses del usuario como palabras clave.
+    """
+
+    def __init__(self, tipo, alta, region, intereses):
+        """ Constructor de la clase Usuario."""
+
+        self.tipo = tipo
+        self.alta = alta
+        self.region = region
+        self.intereses = intereses    
