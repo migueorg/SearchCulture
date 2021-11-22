2d8a9b48ed5b04e3eeb8b5b21b8eeb564237614c
arguellesm <m.arguelles28@gmail.com>
2021-11-22 11:09:03 +0100

    crea clase Respuesta

---
+++ src/respuesta.py
@@ -0,0 +1,29 @@
+class Respuesta:
+    """Respuesta de un usuario a una pregunta.
+
+    Atributos
+    --------
+    id_usuario : int
+        Identificador del usuario que responde.
+    id_pregunta : int
+        Identificador de la pregunta a la que se responde.
+    respuesta : str
+        Respuesta del usuario a la pregunta.
+    """
+
+    def _init_(self, id_usuario, id_pregunta, respuesta):
+        """Constructor de la clase Pregunta.
+
+        Parámetros
+        ----------
+        id_usuario : int
+            Identificador del usuario que responde.
+        id_pregunta : int
+            Identificador de la pregunta a la que se responde.
+        respuesta: str
+            Respuesta del usuario a la pregunta.
+        """
+
+        self.id_usuario = id_usuario
+        self.id_pregunta = id_pregunta
+        self.respuesta = respuesta
