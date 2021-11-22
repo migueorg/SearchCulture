e95bfff21e6d38326d445baa4cc0107097b8a076
arguellesm <m.arguelles28@gmail.com>
2021-11-22 11:08:49 +0100

    añade id a Pregunta

--- src/pregunta.py
+++ src/pregunta.py
@@ -1,8 +1,13 @@
+import itertools
+
+
 class Pregunta:
     """Pregunta destinada a los usuarios.
 
     Atributos
     ---------
+    id : int
+        Identificador único.
     tipo : str
         Tipo de pregunta.
     pregunta : str
@@ -11,6 +16,8 @@ class Pregunta:
         Posibles respuestas a elegir.
     """
 
+    id_iter = itertools.count()
+    
     def __init__(self, tipo, pregunta, respuestas):
         """Constructor de la clase Pregunta.
 
@@ -24,6 +31,7 @@ class Pregunta:
             Posibles respuestas a elegir.
         """
 
+        self.id = next(Pregunta.id_iter)
         self.tipo = tipo
         self.pregunta = pregunta
         self.respuestas = respuestas
