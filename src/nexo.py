4c89441dfcf6885782f0104c3bdceb9870d2072f
Miguel Ángel <milojama@gmail.com>
2022-02-01 16:27:03 +0100

    Implemento getters nexo. Avanza #29

--- src/nexo.py
+++ src/nexo.py
@@ -1,5 +1,5 @@
-import respuesta
-import pregunta
+from src.respuesta import Respuesta
+from src.pregunta import Pregunta
 
 class Nexo:
     """Enlaza preguntas con respuestas, y calcula las ponderaciones de una respuesta.
@@ -10,6 +10,8 @@ class Nexo:
         Pregunta con la que se trabajará.
     respuesta : Respuesta
         Respuesta con la que se trabajará.
+    generos : diccionario str:float
+            Genero y puntuación del mismo
     """
     
     def __init__(self, pregunta, respuesta):
@@ -25,4 +27,27 @@ class Nexo:
 
         self.pregunta = pregunta
         self.respuesta = respuesta
+        self.generos = {'Accion':0.0, 'Drama':0.0, 'Comedia':0.0, 'Sci-fi':0.0}
 
+    def get_pregunta(self):
+        """Getter del atributo pregunta.
+        Corresponde con el objeto pregunta dado."""
+
+        return self.pregunta
+
+    def get_respuesta(self):
+        """Getter del atributo respuesta.
+        Corresponde con el objeto respuesta dado."""
+
+        return self.respuesta
+
+    def get_generos(self):
+        """Getter del atributo generos.
+        Corresponde con los valores calculados para cada genero."""
+
+        return self.generos
+
+    def get_genero_concreto(self, genero):
+        """Devuelve el valor de apego calculado para un genero en concreto"""
+
+        return self.generos[genero]
