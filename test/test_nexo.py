559e147c3e493c339414fa8a4305f3d1c76c6a1a
Miguel Ángel <milojama@gmail.com>
2022-02-01 16:25:42 +0100

    Añado test para cálculos en nexo. Avanza #29

--- test/test_nexo.py
+++ test/test_nexo.py
@@ -37,3 +37,22 @@ def test_generos():
 
     assert_that(nex.get_generos()).is_equal_to(generos_vacio)
     assert_that(nex.get_genero_concreto('Accion')).is_equal_to(0.0)
+
+def test_calculos():
+    """Comprueba que los calculos sobre los generos  en base a la pregunta y respuesta se hacen correctamente"""
+
+    #En futuras versiones es conveniente usar fixtures para la creación de estos objetos
+    pregunta = "¿Que te transmite el final de Titanic?"
+    ponderaciones = {'aburrimiento': 'Accion', 'pena': 'Drama', 'lastima': 'Drama', 'predecible': 'Sci-fi', 'sueño': 'Accion'}
+    preg = Pregunta(pregunta,ponderaciones)
+
+    respuesta = "Mucho aburrimiento y sueño"
+    resp = Respuesta(0,0,respuesta)
+
+    nex = Nexo(preg,resp)
+    generos_vacio = {'Accion':0.0, 'Drama':0.0, 'Comedia':0.0, 'Sci-fi':0.0}
+
+    nex.calcula_generos()
+    assert_that(nex.get_generos()).is_not_equal_to(generos_vacio)
+    assert_that(nex.get_genero_concreto('Accion')).is_greater_than(0.0)
+    assert_that(nex.get_genero_concreto('Accion')).is_less_than(3.0)
